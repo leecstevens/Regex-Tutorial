de18c5a24e8c1e9d1f8a4ca2eca2226f4b0dd9a7
lee <39603399+leecstevens@users.noreply.github.com>
2023-09-08 11:12:48 -0500

    update python - clean up screenshot

--- regex.py
+++ regex.py
@@ -1,7 +1,10 @@
 import re
+intro = 'Testing Regex '
 regex = '^\[(\d{4}|\d{2})(\/|-)\d{2}(\/|-)\d{2} \d{2}:\d{2}(:\d{2})?]'
 timestamps = ['[2023-09-08 12:22:10]','2023-09-08 12:22:10','[2023-09-08 12:22]',
 '[23-09-08 12:22:10]','[23-09-08 12:22]']
+stars = '*' * (len(intro)+len(regex))
+print ('%s\n%s%s\n%s' % (stars, intro, regex, stars))
 
 for i in range (len(timestamps)):
     print ('Testing %s: ' % (timestamps[i]), end = '')
